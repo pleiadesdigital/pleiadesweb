68f8ecfe55155745156dc029b00aadbca71efbd4
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-02-11 10:17:41 -0400

    dead end with menus original 1

--- header.php
+++ header.php
@@ -23,15 +23,18 @@ SITE HEADER
 			<div class="top-header-wrapper">
 				<section class="top-header">
 					<div class="site-branding">
+
 						<a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><img src="http://localhost/~ronyortiz/pleiadesweb/wp-content/themes/pleiadesweb/images/misc/logo-pleiades-digital.png" alt="Pleiades Digital - Online Marketing Agency"></a>
+						
 					</div><!-- class="site-branding" -->
 
 					<div class="main-menu-wrap">
 						<nav id="site-navigation" class="main-navigation" role="navigation">
-							<div class="centerdiv">
+							<!-- <div class="centerdiv"> -->
+								<h1 class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e('<i class="fa fa-align-justify"></i>', 'pleiadesweb'); ?></h1>
 								<!-- MAIN MENU -->
 								<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-							</div>
+							<!-- </div> -->
 								
 						</nav><!-- #site-navigation -->
 					</div><!-- class="menutemp" -->
--- z-sandbox.php
+++
@@ -1,7 +0,0 @@
-<!-- NAVIGATION -->
-
-
-							<h1 class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e('<i class="fa fa-align-justify"></i>', 'pleiadesweb'); ?></h1>
-							<!-- MAIN MENU -->
-							<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-
