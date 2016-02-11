278ea95ae1d9457e6c37da4384f12869efd6237e
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-02-10 22:59:49 -0400

    Alt menu part 1

--- header.php
+++ header.php
@@ -28,7 +28,7 @@ SITE HEADER
 
 					<div class="main-menu-wrap">
 						<nav id="site-navigation" class="main-navigation" role="navigation">
-							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e('<i class="fa fa-align-justify"></i>', 'pleiadesweb'); ?></button>
+							<h1 class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e('<i class="fa fa-align-justify"></i>', 'pleiadesweb'); ?></h1>
 							<!-- MAIN MENU -->
 								<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
 						</nav><!-- #site-navigation -->
--- js/navigation.js
+++ js/navigation.js
@@ -12,7 +12,7 @@
 		return;
 	}
 
-	button = container.getElementsByTagName( 'button' )[0];
+	button = container.getElementsByTagName( 'h1' )[0];
 	if ( 'undefined' === typeof button ) {
 		return;
 	}
