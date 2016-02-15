b8cf2b1d7c8ea416ba3e76d6c7a949065625c468
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-02-15 17:31:05 -0400

    Start with Services content and images

--- front-page.php
+++ front-page.php
@@ -2,13 +2,38 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			
 			<div class="scene" id="welcome">
 				<article class="copycontent">
 					<h1>Bienvenidos a to Pleiades&nbsp;Digital</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus ducimus ratione voluptatum, fugiat sed eaque est nemo! Quas facilis est magni nostrum ad non, dolores ratione, velit nemo esse et.</p>
 				</article><!-- class="copycontent" -->
 			</div><!-- class="scene" -->
+
+			<section class="scene" id="services">
+				<h2>Lo que ofrecemos</h2>
+				<div id="showroom">
+					<section id="webdev">
+						<img src="http://localhost/~ronyortiz/pleiadesweb/wp-content/themes/pleiadesweb/images/services/audio-y-video-para-marketing.jpg" />
+		        <h2>Desarrollo Web para Marketing en Línea</h2>
+		        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+						<a href="#" class="info">Leer más</a>
+	      	</section><!-- id="webdev" -->
+		      <section id="graphdes">
+		      	<img src="http://localhost/~ronyortiz/pleiadesweb/wp-content/themes/pleiadesweb/images/services/audio-y-video-para-marketing.jpg" />
+		        <h2>Imagen Corporativa y Diseño Gráfico</h2>
+		        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+						<a href="#" class="info">Leer más</a>
+		      </section><!-- id="graphdes" -->
+		      <section id="audioprom">
+		      	<img src="http://localhost/~ronyortiz/pleiadesweb/wp-content/themes/pleiadesweb/images/services/audio-y-video-para-marketing.jpg" />
+		        <h2>Audio Promocional para Radio y Video</h2>
+		        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+						<a href="#" class="info">Leer más</a>
+		      </section><!-- id="audioprom" -->
 			
+				</div>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
