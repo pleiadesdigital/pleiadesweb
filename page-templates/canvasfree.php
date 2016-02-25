c30ff2e654bd88574a5d53165aff9135701eeebe
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-02-25 11:51:43 -0400

    Finish contact form w/responsive

--- page-templates/canvasfree.php
+++ page-templates/canvasfree.php
@@ -17,24 +17,28 @@ get_header(); ?>
 
 			<!-- Page title -->
 			<?php the_title("<h2>", "</h2>", true); ?>
+
+			<!-- ADDRESS -->
+			<div id="contact-info">
+				<div class="contact-address">
+					<h3>Dirección</h3>
+					<p>Edif. Torre Ketal, oficina #302</p>
+					<p>Calle 15, Calacoto</p>
+					<p>La Paz - Bolivia</p>
+					<br />
+					<h3>Teléfono / Email</h3>
+					<p>Telf: +591-77799006</p>
+					<p>Correo Electrónico: <a href="mailto:info@pleiadesdigital.com" target="_top">info@pleiadesdigital.com</a></p>
+				</div><!-- .contact-address -->
+			</div><!-- #contact-info -->
+			
 			<!-- Map -->
 			<div id="contact-map">
 				<div id="map-canvas"></div>
 			</div><!-- id=contact-map -->
 
-			<!-- ADDRESS -->
-			<!-- <div id="contact-address">
-				<h3>Información de Contacto</h3>
-				<p>Edif. Torre Ketal, oficina #302</p>
-				<p>Calle 15, Calacoto</p>
-				<p>La Paz - Bolivia</p>
-				<br />
-				<p>Telf: +591-77799006</p>
-				<p>Correo Electrónico: <a href="mailto:info@pleiadesdigital.com" target="_top">info@pleiadesdigital.com</a></p>
-			</div> -->
-
 			<!-- FORM -->
-			<div class="contact-info">
+			<div class="contact-form">
 				<?php the_content(); ?>
 			</div>
 
