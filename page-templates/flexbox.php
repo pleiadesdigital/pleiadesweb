246c963dc5e3ef902233864a577b845413bb2a0d
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-03-08 13:54:16 -0400

    Build single line menu with flexbox tutorial

---
+++ page-templates/flexbox.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Flexbox
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div id="flexbox">
+			<h1>This is the Flexbox sandbox</h1>
+			<section class="menu-section">
+				<h2 class="menu-heading">Single Level Menu</h2>
+				<nav id="single-nav" class="single-nav menu" role="navigation">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Food</a></li>
+						<li><a href="#">Classes</a></li>
+						<li><a href="#">Recipies</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</section>
+		
+
+
+
+
+		</div><!-- id="flexbox" -->
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
