b882c7bf7fdbd61d130166b9099a2c2bba1fdad6
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-03-15 18:03:12 -0400

    Masonry structure done

--- js/masonry-settings.js
+++ js/masonry-settings.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($){
-	$('.grid').masonry({
-		itemSelector: '.grid-item',
-		columnWidth: 200
-		//isFitWidth: true,
-		//isAnimated: true
+	$('#grid').masonry({
+		columnWidth: 400,
+	  itemSelector: '.grid-item',
+	  isFitWidth: true,
+	  isAnimated: true
 	});
 });
