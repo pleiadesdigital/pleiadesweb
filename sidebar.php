bf6d483348d88782f2535b02b7a82e861b23c612
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-05-25 21:47:28 -0400

    WED MAY25 9:47pm

--- sidebar.php
+++ sidebar.php
@@ -14,4 +14,5 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+  <div class="fb-like" data-href="https://www.facebook.com/pleiadesdigital" data-width="350" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
 </aside><!-- #secondary -->
